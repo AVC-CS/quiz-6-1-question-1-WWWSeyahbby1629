de4ac6d802639eeb7310e8881af85bff58d7b5d2
Edosa Eguavoen <freewosa@Edosas-MBP.lan>
2025-04-25 12:54:20 -0700

    done

--- main.hpp
+++ main.hpp
@@ -1,8 +1,38 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int &begin, int &end) {
+    while (true) {
+        cout << "Enter two integers: ";
+        cin >> begin >> end;
+        if (begin < end) {
+            break;
+        } else {
+            cout << "The first value must be less than the second. Please try again." << endl;
+        }
+    }
+}
+
+int getNextPrime(int begin) {
+    int num = begin + 1; 
+    while (!isPrime(num)) {
+        num++;
+    }
+    return num;
+}
+
+int getPrevPrime(int end) {
+    int num = end - 1; 
+    while (!isPrime(num)) {
+        num--;
+    }
+    return num;
+}
